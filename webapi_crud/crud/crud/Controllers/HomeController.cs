3d7ef2c761ec70cf1d4d375292f592c562f431d6
Punita <punita.yadav@tothenew.com>
2020-03-14 19:50:09 +0530

    done

---
+++ webapi_crud/crud/crud/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using crud.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace crud.Controllers
+{
+    public class HomeController : ApiController
+    {
+        Employee employee = new Employee();
+        EmployeeData employeeData = new EmployeeData();
+
+        [HttpGet]
+
+        public IHttpActionResult Selectall()
+        {
+
+            DataSet ds = employeeData.Selectall();
+            return Ok(ds);
+        }
+
+        [HttpGet]
+
+        public IHttpActionResult Selectone(int id)
+        {
+
+            DataSet ds = employeeData.Selectone(id);
+            return Ok(ds);
+        }
+
+        [HttpPost]
+
+        public IHttpActionResult InsertData(Employee employee)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    return Ok(BadRequest());
+                }
+
+                bool result = employeeData.InsertData(employee);
+                if (result)
+                {
+                    return Ok(new { Status = true });
+                }
+                else
+                {
+                    return Ok(new { Status = false, description = " unable to insert data" });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateData(int id, Employee Employee)
+        {
+
+            try
+            {
+                if (employee == null)
+                {
+                    return Ok(BadRequest());
+                }
+                bool result = employeeData.UpdateData(id, employee);
+                if (result)
+                {
+                    return Ok(new { Status = true });
+                }
+                else
+                {
+                    return Ok(new { Status = false, description = "unable to update data" });
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        [HttpDelete]
+        public IHttpActionResult DeleteData(int id)
+        {
+            try
+            {
+                if (employee == null)
+                {
+                    return Ok(BadRequest());
+                }
+                bool result = employeeData.DeleteData(id);
+                if (result)
+                {
+                    return Ok(new { Status = true });
+                }
+                else
+                {
+                    return Ok(new { Status = false, description = "Something went wrong" });
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+
+    }
+}
---
+++ webapi_crud/crud/crud/Models/EmployeeData.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace crud.Models
+{
+    public class EmployeeData
+    {
+        Employee employee = new Employee();
+
+        //Create a WebAPI endpoint to get all employee data from the table.
+        
+        public DataSet Selectall()
+        {
+
+            try
+            {
+               
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Employee", con);
+
+                   
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet);
+                    return dataSet;
+                   
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        //Create a WebAPI endpoint to a single employee data from the table.
+       
+        public DataSet Selectone(int id)
+        {
+
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM Employee where id=" + id, con);
+
+                    con.Open();
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet);
+                    return dataSet;
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        //Create a WebAPI endpoint to create a new record in the database table.
+       
+
+
+        public bool InsertData(Employee employee)
+        {
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+                {
+                    con.Open();
+
+
+                    SqlCommand cmd1 = new SqlCommand("insert into employee(name,salary) values(@name,@salary)", con);
+
+
+
+                    cmd1.Parameters.AddWithValue("@name", employee.Name);
+                    cmd1.Parameters.AddWithValue("@salary", employee.Salary);
+
+                    int row = cmd1.ExecuteNonQuery();
+                    if (row == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        //Create a WebAPI endpoint to delete an employee record in the database table.
+        
+        public bool DeleteData(int id)
+        {
+            try
+            {
+
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+                {
+
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand("Delete from employee where id="+id+" ",con);
+
+                    int row = cmd1.ExecuteNonQuery();
+                    if (row == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        //Create a WebAPI endpoint to update an employee salary in the database table.
+        
+        public bool UpdateData(int id, Employee employee)
+        {
+            try
+            {
+                string query = "UPDATE employee SET  Salary=@Salary WHERE Id=" + id;
+
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query))
+                    {
+
+                       
+                        cmd.Parameters.AddWithValue("@Salary", employee.Salary);
+                        cmd.Connection = con;
+                        con.Open();
+                        int row = cmd.ExecuteNonQuery();
+                        if (row == 1)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+    }
+}
