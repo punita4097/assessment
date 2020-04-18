d0e426fca5f4c7b2e7113b7dc5ee7ab9dd33ec4b
Punita <punita.yadav@tothenew.com>
2020-04-18 16:45:05 +0530

    done

---
+++ BlazorComplete/BlazorAdvance/BlazorAssignment1/Data/Models/NavBarInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAssignment1.Data.Models
+{
+    public class NavBarInfo
+    {
+        [Key]
+        public int NavId { get; set; }
+        [Required(ErrorMessage = "CategoryName is required")]
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage = "Reference is required")]
+        public string Reference { get; set; }
+
+
+    }
+}
---
+++ BlazorComplete/BlazorAdvance/BlazorAssignment1/Data/Services/LoginServices.cs
@@ -0,0 +1,37 @@
+﻿using BlazorAssignment1.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorAssignment1.Data.Services
+{
+    public class LoginServices
+    {
+        private readonly BlazorDbContext _db;
+
+        public LoginServices(BlazorDbContext db)
+        {
+            _db = db;
+        }
+        public string Register(LoginInfo login)
+        {
+            if (_db.Logins.Any(x => x.Name == login.Name))
+            {
+                return "Can not register with same name";
+            }
+            else
+            { 
+                _db.Logins.Add(login);
+                _db.SaveChanges();
+                return "Successfully";
+                
+            }
+
+
+        }
+
+       
+    }
+}
