5f8435a4ccb055b853a1a482b3a15f9e5beb223c
Punita <punita.yadav@tothenew.com>
2020-05-13 13:31:13 +0530

    intor of advance done

---
+++ ReduxAdvancePractice/advance-app/src/store/reducers/result.js
@@ -0,0 +1,47 @@
+import * as actionTypes from '../actions/actionsTypes';
+import {updatedObject} from '../utility.js';
+const initialState={
+    
+    results:[]
+}
+
+const deleteResult =(state,action)=>{
+    const updatedArray=state.results.filter((result => result.id!==action.resultElId));
+    return updatedObject(state,{results:updatedArray});
+
+
+};
+const reducer=(state=initialState,action)=>{
+
+    switch(action.type)
+    {
+        
+
+        case actionTypes.STORE_RESULT:
+            return updatedObject(state,{results:state.results.concat({id: new Date() ,value:action.result})});
+            // return{
+            //         ...state,
+            //         results:state.results.concat({id: new Date() ,value:action.result})
+
+            // }
+
+        case actionTypes.DELETE_RESULT:
+           // const id=2;
+            //const newArray=[...state.results];
+            //newArray.splice(id,1)
+
+            //const updatedArray=state.results.filter((result => result.id!==action.resultElId));
+
+           // return updatedObject(state,{results:updatedArray});
+            // return{
+            //     ...state,
+            //     results:updatedArray
+            // }
+
+            return deleteResult(state,action);
+
+    }
+    return state;
+}
+
+export default reducer;
